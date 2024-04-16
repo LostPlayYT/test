99243b32f3054a3c325caaf402d0b395ccab22a3
Ladislav Hertl <lostplayyt@seznam.cz>
2024-04-16 13:46:11 +0200

    test

---
+++ test.py
@@ -0,0 +1,24 @@
+               #1          #2                #3
+barvy = ['Cyan Modrá', 'Perletove Zelená', 'Fialová']
+
+print("----------------------------------------------")
+print ("Vítejte u konfigurace Lamborghini Huracan STO")
+print("----------------------------------------------")
+
+
+
+print("----------------------------------------------")
+print("Zde si vyberte barvu na auto, vybírat můžete pomocí názvu nebo čísla. Pro ukončení zaddejte příkaz dost")
+print("----------------------------------------------")
+
+
+print("Zde je naše zboží :")
+print(barvy)
+print("----------------------------------------------")
+
+print(" Váše barva:")
+
+
+print("----------------------------------------------")
+barvy = input("Zadejte, jaká barva chcete aby se přidala do košíku: ")
+barva = ['Cyan Modrá', 'Perletove Zelená', 'Fialová']
